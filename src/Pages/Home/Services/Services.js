8c657543f7d8121404675ca8f677019349d5b801
ahatasam2573 <ahatasamul15-3503@diu.edu.bd>
2022-05-20 13:01:16 +0600

    services section created and header added

---
+++ src/Pages/Home/Services/Services.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Services = () => {
+    return (
+        <div className='my-24'>
+            <div className='text-center '>
+                <h3 className='text-primary text-xl uppercase font-bold'>Our Services</h3>
+                <h2 className='text-3xl'>Services We Provide</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Services;
