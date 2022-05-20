a1d75ed7200af90b77e97088cdb88c5f8f487142
ahatasam2573 <ahatasamul15-3503@diu.edu.bd>
2022-05-20 12:44:14 +0600

    info section added by dynamic

---
+++ src/Pages/Home/info.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import InfoCard from './InfoCard';
+import clock from '../../assets/icons/clock.svg'
+import marker from '../../assets/icons/marker.svg'
+import phone from '../../assets/icons/phone.svg'
+
+const info = () => {
+    return (
+        <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 px-12'>
+            <InfoCard description='Open from 9:00AM to 8:00PM' bgClass='bg-gradient-to-r from-secondary to-primary' cardTitle='Opening Hours' img={clock}></InfoCard>
+            <InfoCard description='Near Dhaka College,in front of Dhaka new Market,Dhanmondi,Dhaka-1280' bgClass='bg-accent' cardTitle='Our Location' img={marker}></InfoCard>
+            <InfoCard description='Phone No: 01750-013906' bgClass='bg-gradient-to-r from-secondary to-primary' cardTitle='Contact Us Now' img={phone}></InfoCard>
+        </div>
+    );
+};
+
+export default info;
