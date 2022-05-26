97c455ac541b99bf2578faf678785b443b3f1cbb
ahatasam2573 <ahatasamul15-3503@diu.edu.bd>
2022-05-26 09:55:24 +0600

    updated slot list

--- src/Pages/Appointment/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -1,18 +1,19 @@
 import { format } from 'date-fns';
 import React, { useEffect, useState } from 'react';
+import { useQuery } from 'react-query';
 import BookingModal from './BookingModal';
 import Service from './Service';
+import Loading from '../Shared/Loading'
 
 const AvailableAppointments = ({ date, setDate }) => {
-    const [services, setServices] = useState([]);
     const [treatment, setTreatment] = useState(null);
     const formattedDate = format(date, 'PP');
-    useEffect(() => {
-        fetch(`http://localhost:5000/available?date=${formattedDate}`)
-            .then(res => res.json())
-            .then(data => setServices(data));
-    }, [])
+    const { data: services, isLoading, refetch } = useQuery(['available', formattedDate], () => fetch(`http://localhost:5000/available?date=${formattedDate}`)
+        .then(res => res.json()))
 
+    if (isLoading) {
+        return <Loading></Loading>
+    }
 
     return (
         <div>
@@ -20,10 +21,10 @@ const AvailableAppointments = ({ date, setDate }) => {
             <p className='text-xl text-center text-accent mt-2'>Please Select a service</p>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-24'>
                 {
-                    services.map(service => <Service key={service._id} service={service} setTreatment={setTreatment}></Service>)
+                    services?.map(service => <Service key={service._id} service={service} setTreatment={setTreatment}></Service>)
                 }
             </div>
-            {treatment && <BookingModal date={date} treatment={treatment} setTreatment={setTreatment}></BookingModal>}
+            {treatment && <BookingModal date={date} treatment={treatment} setTreatment={setTreatment} refetch={refetch}></BookingModal>}
         </div>
     );
 };
